28d047db80084003bd45c1bc6a122ab3fa79762a
yusuf-farah <yfarah1577@gmail.com>
2017-10-02 18:24:15 -0400

    added few notes

--- week2_day1/Program.cs
+++ week2_day1/Program.cs
@@ -12,18 +12,58 @@ namespace week2_day1
         {
             //int favoriteNumber
 
-            //int[] favoriteNumbers = { 1, 2, 3, 4, 5, 6 };
+            ////int[] favoriteNumbers = { 1, 2, 3, 4, 5, 6 };
 
-            // string[] studentName = { "Raz", "Monica", "Brian", "Ali", };
+            //// string[] studentName = { "Raz", "Monica", "Brian", "Ali", };
 
-            //Console.WriteLine(favoriteNumbers[0]);
-            //Console.WriteLine(studentName[2]);
-            string[] vactionplace = { "florida", "Bahams", "mugdishu", "china", "uk", "paris", "kenya", "mexico", "somali", "malta" };
+            ////Console.WriteLine(favoriteNumbers[0]);
+            ////Console.WriteLine(studentName[2]);
+            //string[] vactionplace = { "florida", "Bahams", "mugdishu", "china", "uk", "paris", "kenya", "mexico", "somali", "malta" };
 
-            Console.WriteLine(vactionplace[0]);
-            Console.WriteLine(vactionplace[9]);
-            int[] chilAge = { 12, 13, 14 };
-            Console.WriteLine(chilAge[1]);
+            //Console.WriteLine(vactionplace[0]);
+            //Console.WriteLine(vactionplace[9]);
+            //int[] chilAge = { 12, 13, 14 };
+            //Console.WriteLine(chilAge[1]);
+            ////decimal[] gpaScore = { 2.5, 2.6, 3.2, 3.9, };
+
+            //int[] numbers = new int[4];
+
+            //numbers[0] = 43;
+            //numbers[1] = 12;
+            //Console.WriteLine("please enter a number");
+            //numbers[2] = int.Parse(Console.ReadLine());
+            //numbers[3] = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(numbers[1]);
+            /////to manipulate and replace the data inside the array
+
+            string[] daysOfweek = new String[7];
+            daysOfweek[0] = "sun";
+            daysOfweek[1] = "mon";
+            daysOfweek[2] = "tues";
+            daysOfweek[3] = "wed";
+            daysOfweek[4] = "thu";
+            DayOfWeek[5] = "frdy";
+            daysOfweek[6] = "stu";
+           
+
+            //int[] numberStudent = new int[5];
+
+            //Console.WriteLine(daysOfweek.Length);
+            //string[] lastname = { "farah" };
+            //Console.WriteLine(lastname.Length);
+
+
+            //int index = examplestringarray.Length;
+            //index = index - 2;
+            //Console.WriteLine(examplestringarray[index]);
+
+            string[] examplestringarray = { " this ", "is", "an", "array", "of", "works" };
+
+
+            Console.WriteLine(examplestringarray[examplestringarray.Length - 1]);
+
+
 
         }
 
