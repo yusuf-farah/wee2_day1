a488c73da684c42a9995e375610a4894637915ef
yusuf-farah <yfarah1577@gmail.com>
2017-10-02 11:53:43 -0400

    added few more codes

--- week2_day1/Program.cs
+++ week2_day1/Program.cs
@@ -10,6 +10,23 @@ namespace week2_day1
     {
         static void Main(string[] args)
         {
+            //int favoriteNumber
+
+            //int[] favoriteNumbers = { 1, 2, 3, 4, 5, 6 };
+
+            // string[] studentName = { "Raz", "Monica", "Brian", "Ali", };
+
+            //Console.WriteLine(favoriteNumbers[0]);
+            //Console.WriteLine(studentName[2]);
+            string[] vactionplace = { "florida", "Bahams", "mugdishu", "china", "uk", "paris", "kenya", "mexico", "somali", "malta" };
+
+            Console.WriteLine(vactionplace[0]);
+            Console.WriteLine(vactionplace[9]);
+            int[] chilAge = { 12, 13, 14 };
+            Console.WriteLine(chilAge[1]);
+
         }
+
+
     }
 }
