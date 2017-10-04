6303b2b58d441d047e2463a4e067ba9e813f8ac7
yusuf-farah <yfarah1577@gmail.com>
2017-10-04 14:00:38 -0400

    Made adjustments

--- week2_day1/Program.cs
+++ week2_day1/Program.cs
@@ -37,15 +37,15 @@ namespace week2_day1
             //Console.WriteLine(numbers[1]);
             /////to manipulate and replace the data inside the array
 
-            string[] daysOfweek = new String[7];
-            daysOfweek[0] = "sun";
-            daysOfweek[1] = "mon";
-            daysOfweek[2] = "tues";
-            daysOfweek[3] = "wed";
-            daysOfweek[4] = "thu";
-            DayOfWeek[5] = "frdy";
-            daysOfweek[6] = "stu";
-           
+            //string[] daysOfweek = new String[7];
+            //daysOfweek[0] = "sun";
+            //daysOfweek[1] = "mon";
+            //daysOfweek[2] = "tues";
+            //daysOfweek[3] = "wed";
+            //daysOfweek[4] = "thu";
+            //DayOfWeek[5] = "frdy";
+            //daysOfweek[6] = "stu";
+
 
             //int[] numberStudent = new int[5];
 
@@ -58,10 +58,42 @@ namespace week2_day1
             //index = index - 2;
             //Console.WriteLine(examplestringarray[index]);
 
-            string[] examplestringarray = { " this ", "is", "an", "array", "of", "works" };
+            //string[] examplestringarray = { " this ", "is", "an", "array", "of", "works" };
 
 
-            Console.WriteLine(examplestringarray[examplestringarray.Length - 1]);
+            //Console.WriteLine(examplestringarray[examplestringarray.Length - 1]);
+
+            //int arraysize = int.Parse(Console.ReadLine());
+            //int[] newAray = new int[arraysize];
+
+            //Console.WriteLine(" the array is of length" + newAray.Length);
+            
+            //string[] birthMonths = { "october", "july", "october", "may" };
+            //Console.WriteLine(Array.IndexOf(birthMonths, "october"));
+            //int[] numbers = { 2, 5, 9, 4, 3, 4, 6, 5 };
+            //Console.WriteLine(Array.LastIndexOf(numbers, 4));
+            //Array.Reverse(numbers);
+            //Console.WriteLine(numbers[0]);
+            //Array.Sort(numbers);
+            //Console.WriteLine(numbers[numbers.Length - 1]);
+            /*Declare and initialize a string array of first names. Choose a name to find and print the IndexOf*/
+
+            string[] firstNames = { "yusuf", "kevin", "ali", "david" };
+            Console.WriteLine(Array.IndexOf(firstNames, "david"));
+
+
+            /*Create an array of lucky numbers. Each number should repeat at least once. Choose a lucky number, find and print the last index of it*/
+            int[] luckyNumbers = { 1, 3, 3, 3,1 ,1 ,2,2,2 };
+            Console.WriteLine(Array.LastIndexOf(luckyNumbers, 3));
+            /*Create an array of alphabet characters. Print the first index of the array. Now reverse the array and print the first index*/
+            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'd' };
+            Console.WriteLine(alphabet[0]);
+            Array.Reverse(alphabet);
+            Console.WriteLine(alphabet[0]);
+
+            S
+
+
 
 
 
